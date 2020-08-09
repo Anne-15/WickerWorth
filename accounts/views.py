ba6d15315c820e2999112fd4c34b2b2ac4c74624
Anne-15 <wariiyuanne@gmail.com>
2020-08-09 18:25:16 +0300

    Styles page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
+from accounts.forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Your account has been created successfully")
+            return redirect ('login')
+    else:
+        form = UserRegisterForm()
+
+    dict = {'form':form}
+    return render(request, 'accounts/register.html')
