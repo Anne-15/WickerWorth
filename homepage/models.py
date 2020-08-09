5b5c1fb5860e0a62b0d12b8b111e47be615a6e83
Anne-15 <wariiyuanne@gmail.com>
2020-08-09 11:35:51 +0300

    Created first app, the home page app

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class PersonInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField
+    phone_number = models.IntegerField
+
+    def __str__(self):
+        return self.first_name
+
+class Products(models.Model):
+    id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=200)
+    product_description = models.CharField(max_length=1000)
+    phone_number = models.ForeignKey(PersonInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name
