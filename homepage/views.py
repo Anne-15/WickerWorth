bfae9839751e597ad7826d4029163b7499e13682
Anne-15 <wariiyuanne@gmail.com>
2020-08-09 12:25:31 +0300

    Adding into templates

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from  django.template import loader
+from django.shortcuts import render
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World.")
+    return render(request, 'homepage/index.html')
