139b58d259699f69da60d8ea6bc9dc780f320e99
Anne-15 <wariiyuanne@gmail.com>
2020-08-10 15:30:19 +0300

    search functionality

--- homepage/models.py
+++ homepage/models.py
@@ -20,3 +20,11 @@ class Products(models.Model):
 
     def __str__(self):
         return self.product_name
+
+class Search(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.search
+
--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from  django.template import loader
 from django.shortcuts import render
+from bs4 import beautifulsoup
+import request
 
 # Create your views here.
 
 def index(request):
     return render(request, 'homepage/index.html')
 
+def search(request):
+    new_search = request.POST.get('new_search')
+    print(new_search)
+    stuff_for_frontend = {
+        'new_search':new_search,
+    }
+    return render(request, 'homepage/search.html', stuff_for_frontend)
